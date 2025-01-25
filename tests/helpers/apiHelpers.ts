04d2c281c5b1dcb339be91c60191442889e6dfc9
Steven Boutcher <boutchersj@gmail.com>
2025-01-25 16:01:35 -0600

    Use API key as a secret in the helper

--- tests/helpers/apiHelpers.ts
+++ tests/helpers/apiHelpers.ts
@@ -1,5 +1,14 @@
 // apiHelper.ts
 import { request } from '@playwright/test';
+// Import the dotenv library
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Now you can access your API key
+const apiKey = process.env.RAPIDAPI_KEY;
+
 
 export const fetchActiveATS = async (title: string, location: string) => {
   const context = await request.newContext();
@@ -7,7 +16,7 @@ export const fetchActiveATS = async (title: string, location: string) => {
   const response = await context.get('https://active-jobs-db.p.rapidapi.com/active-ats-7d', {
     headers: {
       'x-rapidapi-host': 'active-jobs-db.p.rapidapi.com',
-      'x-rapidapi-key': 'YOUR_RAPIDAPI_KEY', // Use environment variable for security
+      'x-rapidapi-key': `${apiKey}`, // Use environment variable for security
     },
     params: {
       title_filter: `"${title}"`,
